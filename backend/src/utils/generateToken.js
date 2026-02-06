a820c88a9759c8c23e43548b1c85af2ca030fad9
Aanshi Dobariya <aanshidobariya.107@gmail.com>
2026-02-06 14:00:11 +0530

    push

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -6,7 +6,11 @@ export const generateToken = (id,res) => {
     })
 
     res.cookie('token', token, {
-        maxAge: 30 * 24 * 60 * 60 * 1000
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,     // Prevents XSS attacks
+        secure: true,       // Required for sameSite: 'none' (works on HTTPS)
+        sameSite: 'none',   // Allows cookie to be sent from Vercel to Render
+        path: '/'
     })
 
 
